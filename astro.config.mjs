4f1ca1cf4f10c7d2e0fb78eb558c4f6ddf2e48df
Irving Dinh <irving.dinh@gmail.com>
2023-06-21 20:55:47 +0700

    Initial commit (cont.)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    mdx(),
-    tailwind(),
-  ]
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), mdx(), react(), tailwind()]
 });
